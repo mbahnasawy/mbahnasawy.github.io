b849e423bce7bf4f9db3dfbe18e43a084461c152
Moustafa Bahnasawy <moustafa.a.galal@gmail.com>
2020-11-01 23:35:58 -0600

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -6,9 +6,11 @@ function max(n1,n2){
         return n2;
     } 
 }
+
 function maxOfThree(n1,n2,n3){
    return max(max(n1,n2),n3);
 }
+
 function isVowel(char){
    return 'AEIOU'.indexOf(char.toUpperCase()) !== -1;
 }
@@ -16,11 +18,13 @@ function isVowel(char){
 function sum(...arr) {
     return arr.reduce((accumulator,item)=> {return accumulator+item});
 }
+
 function multiply(...arr) {
     return arr.reduce(
         (accumulator,item)=>{ return accumulator*item}
     );
 }
+
 function reverse(word) {
     let reversedWord = "";
     for(let i = word.length-1;i>=0;--i){
@@ -28,22 +32,23 @@ function reverse(word) {
     }
     return reversedWord;
 }
+
 function findLongestWord(...words) {
     return words.reduce((longestWord="",word)=>{return max(longestWord.length,word.length)})
 }
+
 function filterLongWords(i,...words) {
     let str = [];
     str=words.filter(word=>word.length>i);
     return str;
 }
+
 function findShortestWord(...words) {
     return words.reduce((shortestWord,currentword)=>
     {
         return currentword.length < shortestWord.length ? currentword : shortestWord;
     })
 }
-
-//slides map/filter/reduce
 
 const mapExample = (...arr)=>{ return arr.map(elem=>elem*10)};
 const filterExample= (...arr)=>{return arr.filter(num=>num===3)};
@@ -60,6 +65,7 @@ function myFunctionArrayTest(expected, found) {
         return "TEST FAILED.  Expected " + expected + " found " + found();
     }
 }
+
 function myFunctionTest(expected,found){
     console.assert(expected===found(),"TEST FAILED.  Expected " + expected + " found " + found());
     if(expected===found()){
@@ -73,16 +79,15 @@ function myFunctionTest(expected,found){
 console.log("Expected output of max(20,10) is 20 "+myFunctionTest(20, ()=>max( 20, 10)));
 console.log("Expected output of maxOfThree(5,4,44) is 44 "+myFunctionTest(44, ()=>maxOfThree( 5,4, 44)));
 console.log("Expected output of maxOfThree(55,4,44) is 55 "+myFunctionTest(55, ()=>maxOfThree( 55,4, 44)));
-console.log("Expected output of maxOfThree(55,4,44) is 55 "+myFunctionTest(4, ()=>maxOfThree( 55,4, 44)));
 
 console.log("\n");
-console.log("Expected output of isVowel(a) is true "+myFunctionTest(true, ()=>isVowel("a")));
-console.log("Expected output of isVowel(k) is false "+myFunctionTest(false, ()=>isVowel("k")));
-console.log("Expected output of isVowel(u) is true "+myFunctionTest(false, ()=>isVowel("u")));
+console.log("Expected output of isVowel(a) is true "+myFunctionTest(true, ()=>isVowel("i")));
+console.log("Expected output of isVowel(k) is false "+myFunctionTest(false, ()=>isVowel("m")));
+console.log("Expected output of isVowel(u) is true "+myFunctionTest(true, ()=>isVowel("u")));
 
 console.log("\n");
 console.log("Expected output of sum([1,2,3,4]) is 10 "+myFunctionTest(10, ()=>sum(1,2,3,4)));
-console.log("Expected output of sum([3,3,3,4]) is 13 "+myFunctionTest(10, ()=>sum(3,3,3,4)));
+console.log("Expected output of sum([3,3,3,4]) is 13 "+myFunctionTest(13, ()=>sum(3,3,3,4)));
 
 console.log("\n");
 console.log("Expected output of multiply([4,8,9,7]) is 2016 "+myFunctionTest(2016, ()=>multiply(4,8,9,7)));
@@ -93,12 +98,7 @@ console.log("Expected output of reverse(\"nice\") is ecin "+myFunctionTest("ecin
 console.log("Expected output of reverse(\"okay\") is yako "+myFunctionTest("il", ()=>reverse("okay")));
 
 console.log("\n");
-console.log("Expected output of findLongestWord([okay,kareem,vowel]) is 5 "+myFunctionTest(5, ()=>findLongestWord("okay","kareem","vowel")));
-console.log("Expected output of findLongestWord([okay,kareem,vowel]) is 5 "+myFunctionTest(3, ()=>findLongestWord("okay","kareem","vowel")));
-
-console.log("\n");
-console.log("Expected output of filterLongWords(4,[okay,kareem,vowel]) is [kareem,vowel] "+myFunctionArrayTest(['kareem','vowel'], ()=>filterLongWords(4,'okay','kareem','vowel')));
-console.log("Expected output of filterLongWords(4,[okay,kareem,vowel]) is [kareem,vowel] "+myFunctionArrayTest(['kareem'], ()=>filterLongWords(4,'okay','kareem','vowel')));
+console.log("Expected output of findLongestWord([okay,Moustafa,vowel]) is 8 "+myFunctionTest(8, ()=>findLongestWord("okay","Moustafa","vowel")));
 
 console.log("\n");
 console.log("Expected output of mapExample([5,8,3,1]) is [50,80,30,10] "+myFunctionArrayTest([50,80,30,10], ()=>mapExample(5,8,3,1)));
