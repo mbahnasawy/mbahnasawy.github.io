25bde875ac9055d535d662fec71ea2d5df8d681e
Moustafa Bahnasawy <moustafa.a.galal@gmail.com>
2020-11-08 02:08:36 -0600

    Update ScopeEncapsulation.js

--- js/ScopeEncapsulation.js
+++ js/ScopeEncapsulation.js
@@ -1,109 +1,62 @@
-function max(n1,n2){
-    if(n1>n2){
-        return n1;
+String.prototype.filter=function(...str){
+    return this.split(' ').filter((word)=>str.indexOf(word)<0).join(" ");
+};
+
+describe("String Filter",()=>{
+
+    it("Filter Test",()=>{
+        assert.equal("This house is not nice!".filter("not"),"This house is nice!"); 
+    });
+
+});
+
+Array.prototype.bubbleSort= function(){
+    let len =this.length;
+    for(let i=0;i<len;++i){
+        for(let j=0;j<len;++j){
+            if(this[j]>this[j+1]){
+                let temp = this[j];
+                this[j]=this[j+1];
+                this[j+1]=temp;
+            }
+        }
     }
-    else {
-        return n2;
-    } 
+    return this;
+};
+
+describe("Bubble Sort Array",()=>{
+    it("Buuble sort test",()=>{
+        assert.deepEqual([6,4,0, 3,-2,1].bubbleSort(),[-2,0,1,3,4,6]);
+    });
+});
+
+function Person(name,age){
+    this.name=name;
+    this.age =age;
+}
+Person.prototype.describe=function(){
+    return this.name+", "+this.age+" years old.";
 }
 
-function maxOfThree(n1,n2,n3){
-   return max(max(n1,n2),n3);
+function Teacher(name,age){
+    this.name=name;
+    this.age=age;
 }
 
-function isVowel(char){
-   return 'AEIOU'.indexOf(char.toUpperCase()) !== -1;
+Teacher.prototype.__proto__=Person.prototype;
+
+Teacher.prototype.teach=function(subject){
+    return this.name+" is now teaching "+subject;
 }
 
-function sum(...arr) {
-    return arr.reduce((accumulator,item)=> {return accumulator+item});
-}
+let teacher1 = new Teacher("Sami Taha",30);
+console.log(teacher1.teach("WAP"));
 
-function multiply(...arr) {
-    return arr.reduce(
-        (accumulator,item)=>{ return accumulator*item}
-    );
-}
+describe("Person Teacher Inheritance",()=>{
+    it("Encapsulation test",()=>{
+        assert.equal(new Teacher("Sami Taha",30).teach("WAP"),"Sami Taha is now teaching WAP");
+    });
 
-function reverse(word) {
-    let reversedWord = "";
-    for(let i = word.length-1;i>=0;--i){
-        reversedWord += word[i];
-    }
-    return reversedWord;
-}
+});
 
-function findLongestWord(...words) {
-    return words.reduce((longestWord="",word)=>{return max(longestWord.length,word.length)})
-}
-
-function filterLongWords(i,...words) {
-    let str = [];
-    str=words.filter(word=>word.length>i);
-    return str;
-}
-
-function findShortestWord(...words) {
-    return words.reduce((shortestWord,currentword)=>
-    {
-        return currentword.length < shortestWord.length ? currentword : shortestWord;
-    })
-}
-
-const mapExample = (...arr)=>{ return arr.map(elem=>elem*10)};
-const filterExample= (...arr)=>{return arr.filter(num=>num===3)};
-const reduceExample = (...arr)=>{return arr.reduce(
-    (accumulator,elem)=>{return accumulator*elem}
-)};
-
-
-function myFunctionArrayTest(expected, found) {
-    console.assert(JSON.stringify(expected) === JSON.stringify(found()), "TEST FAILED.  Expected " + expected + " found " + found());
-    if (JSON.stringify(expected) === JSON.stringify(found())) {
-        return "TEST SUCCEEDED";
-    } else {
-        return "TEST FAILED.  Expected " + expected + " found " + found();
-    }
-}
-
-function myFunctionTest(expected,found){
-    console.assert(expected===found(),"TEST FAILED.  Expected " + expected + " found " + found());
-    if(expected===found()){
-        return "TEST SUCCEEDED";
-    }
-    else{
-        return "TEST FAILED.  Expected " + expected + " found " + found();
-    }
-}
-
-console.log("Expected output of max(20,10) is 20 "+myFunctionTest(20, ()=>max( 20, 10)));
-console.log("Expected output of maxOfThree(5,4,44) is 44 "+myFunctionTest(44, ()=>maxOfThree( 5,4, 44)));
-console.log("Expected output of maxOfThree(55,4,44) is 55 "+myFunctionTest(55, ()=>maxOfThree( 55,4, 44)));
-
-console.log("\n");
-console.log("Expected output of isVowel(i) is true "+myFunctionTest(true, ()=>isVowel("i")));
-console.log("Expected output of isVowel(m) is false "+myFunctionTest(false, ()=>isVowel("m")));
-console.log("Expected output of isVowel(u) is true "+myFunctionTest(true, ()=>isVowel("u")));
-
-console.log("\n");
-console.log("Expected output of sum([1,2,3,4]) is 10 "+myFunctionTest(10, ()=>sum(1,2,3,4)));
-console.log("Expected output of sum([3,3,3,4]) is 13 "+myFunctionTest(13, ()=>sum(3,3,3,4)));
-
-console.log("\n");
-console.log("Expected output of multiply([4,8,9,7]) is 2016 "+myFunctionTest(2016, ()=>multiply(4,8,9,7)));
-console.log("Expected output of multiply([5,2,3,1]) is 30 "+myFunctionTest(30, ()=>multiply(5,2,3,1)));
-
-console.log("\n");
-console.log("Expected output of reverse(\"nice\") is ecin "+myFunctionTest("ecin", ()=>reverse("nice")));
-
-console.log("\n");
-console.log("Expected output of findLongestWord([okay,Moustafa,vowel]) is 5 "+myFunctionTest(5, ()=>findLongestWord("okay","Moustafa","vowel")));
-
-console.log("\n");
-console.log("Expected output of mapExample([5,8,3,1]) is [50,80,30,10] "+myFunctionArrayTest([50,80,30,10], ()=>mapExample(5,8,3,1)));
-
-console.log("\n");
-console.log("Expected output of filterExample([5,3,8,5,3,22]) is [3,3] "+myFunctionArrayTest([3,3], ()=>filterExample(5,3,8,5,3,22)));
-console.log("\n");
-console.log("Expected output of filterExample([1,5,4,6]) is 120 "+myFunctionTest(120, ()=>reduceExample(1,5,4,6)));
 
